b439c6413c6b9f85c691069763b323c056651707
ToryxDev <toryx.dev@gmail.com>
2025-07-04 23:51:08 +0300

    Initial commit

---
+++ src/main/java/net/cinny/cinnymod/CinnyModClient.java
@@ -0,0 +1,10 @@
+package net.cinny.cinnymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CinnyModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/cinny/cinnymod/CinnyModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.cinny.cinnymod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CinnyModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
